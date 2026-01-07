46b3948dd062d1e8f4587e67a3b2558819e83fc7
Roberto Traber <roberto@MacBook-Pro-von-Roberto.local>
2026-01-07 17:29:54 -0300

    sales

--- sales.sql
+++ sales.sql
@@ -2,7 +2,8 @@ Select
 	sp.name.
 	sp.position,
 	sp.salary,
-	SUM(a.amount) AS total_sales
+	SUM(a.amount) AS total_sales,
+	RANK() OVER (ORDER BY SUM(a.amount) DESC) as sales_rank
 FROM
 	t_sales_person sp
 JOIN 
